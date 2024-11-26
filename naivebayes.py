f6852213d2cb9cda9965037e59edc77f1a521f13
22026536 <22026536>
2024-11-26 22:51:08 +0700

    quang

--- naivebayes.py
+++ naivebayes.py
@@ -1,3 +1,43 @@
+from fastapi import FastAPI, Request
+import pandas as pd
+from pymongo import MongoClient
+
+# Khởi tạo app
+app = FastAPI()
+
+client = MongoClient("mongodb+srv://sangvo22026526:5anG15122003@cluster0.rcd65hj.mongodb.net/web_project")
+db = client["anime_tango2"]
+
+# Tải dữ liệu từ MongoDB
+df_ratings = pd.DataFrame(list(db["UserRating"].find()))
+df_anime = pd.DataFrame(list(db["Anime"].find()))
+df_episode = pd.DataFrame(list(db["AnimeEpisode"].find()))
+
+
+############################
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+# Chuẩn bị dữ liệu cho Decision Tree và Naive Bayes
+label_encoder = LabelEncoder()
+df_anime['Name_encoded'] = label_encoder.fit_transform(df_anime['Name'])  # Mã hóa tên anime
+
+# Kết hợp thông tin xếp hạng vào dữ liệu phim
+df_merged = df_ratings.merge(df_anime, on="Anime_id")
+X = df_merged[['User_id', 'Rating']]  # Đặc trưng đầu vào
+y = df_merged['Name_encoded']         # Nhãn đầu ra
+
+# Tách tập dữ liệu thành train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Mô hình Naive Bayes
+naive_bayes_model = MultinomialNB()
+naive_bayes_model.fit(X_train, y_train)
+
+
+
 def recommend_by_naive_bayes(user_id, n):
     """
     Gợi ý phim sử dụng mô hình Naive Bayes, trả về toàn bộ thông tin anime.
